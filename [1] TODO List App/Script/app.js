cd3d4765575c199331dc3e3ca25f24a2b21c61d4
Apoorv <vapoorve@gmail.com>
2019-03-30 17:20:08 +0530

    TODO List App Using AngularJs

--- [1] TODO List App/Script/app.js
+++ [1] TODO List App/Script/app.js
@@ -1,18 +1,36 @@
 angular.module("TodoList",[])
-.controller('mainCCtrl',function($scope){
+.controller('mainCCtrl',function($scope,DataService,$http){
+    $scope.servicefun = DataService.servicefun;
+
     $scope.learningngChange = function(){
         console.log("input change ");
     }
 
-    $scope.todos = [
-        { "data" : "testing 1"},
-        { "data" : "testing 2"},
-        { "data" : "testing 3"},
-        { "data" : "testing 4"},
-        { "data" : "testing 4"},
-        { "data" : "testing 5"},
-        { "data" : "testing 5"},
-    ]
+    DataService.gettodo(function(response){
+    		console.log(response.data);
+    });
+
+        $scope.todos =[
+            { "data" : "testing 1"},
+            { "data" : "testing 2"},
+            { "data" : "testing 3"},
+            { "data" : "testing 4"},
+            { "data" : "testing 4"},
+            { "data" : "testing 5"},
+            { "data" : "testing 5"},
+        ]
+
+
 })
+    .service('DataService',function($http){
 
+        this.servicefun = function(){
+            console.log("This is service Fun");
+        }
 
+        this.gettodo = function(callback){
+        	$http.get('/mocks/todo.json')
+        .then(callback);
+        }
+
+    });
