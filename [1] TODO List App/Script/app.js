0514583f1542a137133c3261173537f803b8c8e5
Apoorv <vapoorve@gmail.com>
2019-03-30 14:19:45 +0530

    TODO List App

--- [1] TODO List App/Script/app.js
+++ [1] TODO List App/Script/app.js
@@ -1,1 +1,17 @@
-angular.module("TodoList",[]);
+angular.module("TodoList",[])
+.controller('mainCCtrl',function($scope){
+    $scope.testctrl = function(){
+        console.log("testinng controller");
+    }
+    $scope.todos = [
+        { "data" : "testing 1"},
+        { "data" : "testing 2"},
+        { "data" : "testing 3"},
+        { "data" : "testing 4"},
+        { "data" : "testing 4"},
+        { "data" : "testing 5"},
+        { "data" : "testing 5"},
+    ]
+})
+
+
--- [1] TODO List App/Style/hellow-world.js
+++
@@ -1,6 +0,0 @@
-angular.module("TodoList")
-.directive('helloWorld',function(){
-    return {
-        template : "This is My First Angular App "
-    };
-});
