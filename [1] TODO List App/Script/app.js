2a7387bbbf4d6081f5e4fd60943dfa95f94ad4cf
Apoorv <vapoorve@gmail.com>
2019-03-31 13:43:50 +0530

    TODO List App Using AngularJs

--- [1] TODO List App/Script/app.js
+++ [1] TODO List App/Script/app.js
@@ -1,6 +1,5 @@
 angular.module("TodoList",[])
 .controller('mainCCtrl',function($scope,DataService,$http){
-    $scope.servicefun = DataService.servicefun;
 
     $scope.learningngChange = function(){
         console.log("input change ");
@@ -11,18 +10,35 @@ angular.module("TodoList",[])
         $scope.todos = response.data;
     });
 
+    $scope.deletetodo = function(todo,index){
+        DataService.deletetodo(todo);
+        $scope.todos.splice(index,1);
+    }
+
+    $scope.savetodo = function(todo){
+        DataService.savetodo(todo);
+
+    }
+
+    $scope.addtodo = function(){
+        var todo = {data : "This is new todo"};
+        console.log(todo);
+        $scope.todos.push(todo);
+    };
 
 
 })
     .service('DataService',function($http){
-
-        this.servicefun = function(){
-            console.log("This is service Fun");
-        }
 
         this.gettodo = function(callback){
         	$http.get('https://raw.githubusercontent.com/Apoorve8055/Quick_Start_With_Angularjs/master/%5B1%5D%20TODO%20List%20App/mocks/todo.json')
         .then(callback);
         }
 
+        this.deletetodo = function(todo){
+            console.log("The "+ todo.data + " Has been deleted !");
+        }
+        this.savetodo = function(todo){
+           console.log("The "+ todo.data + " Has been Saved ");
+        }
     });
